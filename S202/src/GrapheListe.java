bdc0571bd06774a4285a0f02cd190f42a795739a
tuline <tuline.leveque@gmail.com>
2024-05-28 17:13:49 +0200

    question 4 suivants, listeNoeuds et getIndice

--- S202/src/GrapheListe.java
+++ S202/src/GrapheListe.java
@@ -7,30 +7,34 @@ public class GrapheListe implements Graphe {
     private ArrayList<String> noeuds;
     //a liste des Arcs partant de chaque noeud du graphe
     private ArrayList<Arcs> adjacence;
+
+    /**
+     * methode getIndice
+     * @param n noeud
+     * @return indice du noeud passe en parametre
+     */
+    public int getIndice(String n) {
+        boolean trouve = false;
+        int i = 0;
+        int indiceN = 0;
+        while ((!trouve)&&(i<this.noeuds.size())) {
+            if (this.noeuds.get(i).compareTo(n) == 0) {
+                trouve = true;
+                indiceN = i;
+            }
+            i++;
+        }
+        return indiceN;
+    }
     @Override
     public List<String> listeNoeuds() {
-        return null;
-
-
-
-
-
-
-        
-
-
-
-
-
-
-
-
-
+        return this.noeuds;
     }
 
     @Override
     public List<Arc> suivants(String n) {
-        return null;
+        int indice = this.getIndice(n);
+        return this.adjacence.get(indice).getArcs();
     }
 
 
