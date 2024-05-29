1405c7c954327123a7994d24d4503a1a0ab06bfe
tuline <tuline.leveque@gmail.com>
2024-05-29 08:22:02 +0200

    création dossier et classe test à compléter

---
+++ S202/test/GrapheListeTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GrapheListeTest {
+
+    @Test
+    void getIndice() {
+    }
+
+    @Test
+    void listeNoeuds() {
+    }
+
+    @Test
+    void suivants() {
+    }
+
+    @Test
+    void ajouterArc() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
