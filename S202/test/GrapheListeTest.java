2f8f2ce3a67d447878e4120ad4410a68f3a129a8
tuline <tuline.leveque@gmail.com>
2024-05-29 09:21:42 +0200

    classe test

--- S202/test/GrapheListeTest.java
+++ S202/test/GrapheListeTest.java
@@ -1,21 +1,60 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GrapheListeTest {
 
     @Test
     void getIndice() {
+        GrapheListe g = new GrapheListe();
+        g.ajouterArc("A", "B", 12);
+        g.ajouterArc("A", "D", 87);
+        g.ajouterArc("C", "A", 19);
+
+        assertEquals(1, g.getIndice("B"));
+    }
+    @Test
+    void getIndiceDebut() {
+        GrapheListe g = new GrapheListe();
+        g.ajouterArc("A", "B", 12);
+        g.ajouterArc("A", "D", 87);
+        g.ajouterArc("C", "A", 19);
+
+        assertEquals(0, g.getIndice("A"));
     }
 
     @Test
-    void listeNoeuds() {
+    void getIndiceFin() {
+        GrapheListe g = new GrapheListe();
+        g.ajouterArc("A", "B", 12);
+        g.ajouterArc("A", "D", 87);
+        g.ajouterArc("C", "A", 19);
+
+        assertEquals(3, g.getIndice("C"));
+    }
+
+    @Test
+    void getIndiceInexistant() {
+        GrapheListe g = new GrapheListe();
+        g.ajouterArc("A", "B", 12);
+        g.ajouterArc("A", "D", 87);
+        g.ajouterArc("C", "A", 19);
+
+        assertEquals(-1, g.getIndice("H"));
     }
 
     @Test
     void suivants() {
-    }
+        GrapheListe g = new GrapheListe();
+        g.ajouterArc("A", "B", 12);
+        g.ajouterArc("A", "D", 87);
+        g.ajouterArc("C", "A", 19);
 
+        assertEquals("B",g.suivants("A").)
+    }
+/*
     @Test
     void ajouterArc() {
     }
@@ -23,4 +62,6 @@ class GrapheListeTest {
     @Test
     void testToString() {
     }
+
+ */
 }
