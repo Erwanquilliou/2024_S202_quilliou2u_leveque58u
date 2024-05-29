21172e079694d864a425874ad774b5a6b8bc6601
tuline <tuline.leveque@gmail.com>
2024-05-29 11:53:09 +0200

    debut Dijkstra

--- S202/src/Dijkstra.java
+++ S202/src/Dijkstra.java
@@ -1,12 +1,14 @@
+import java.util.List;
+
 public class Dijkstra {
     /*Entrees :
     G un graphe oriente avec une ponderation positive des arcs (cout ou poids)
-    A un sommet (d´epart) de G
+    A un sommet (depart) de G
     Debut
     Q <- {} // utilisation d’une liste de noeuds a traiter
     Pour chaque sommet v de G faire
     v.valeur <- Infini
-    v.parent <- Ind´efini
+    v.parent <- Indefini
     Q <- Q U {v} // ajouter le sommet v a la liste Q
     Fin Pour
     A.valeur <- 0
@@ -25,6 +27,13 @@ public class Dijkstra {
     Fin Tant que
     Fin*/
     public void resoudre(GrapheListe g, String depart) {
-        
+        Valeur v = new Valeur();
+        List<String> listeNoeuds = g.listeNoeuds();
+        for (int sommet=0; sommet<listeNoeuds.size(); sommet++) {
+            v.setValeur(listeNoeuds.get(sommet),Double.MAX_VALUE);
+        }
+        v.setValeur(depart,0);
+
+
     }
 }
